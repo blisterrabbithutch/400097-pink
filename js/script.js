b72b604b5431d0298704d9340e345e60d6120514
blisterrabbithutch <blisterrabbithutch@gmail.com>
2017-06-24 01:08:56 +0500

    финальная версия

--- js/script.js
+++ js/script.js
@@ -1,19 +1,17 @@
-
-
 var navMain = document.querySelector('.main-nav');
-    var navToggle = document.querySelector('.main-nav__toggle');
-    var HeaderToggle = document.querySelector('.main-header');
+var navToggle = document.querySelector('.main-nav__toggle');
+var HeaderToggle = document.querySelector('.main-header');
 
-    HeaderToggle.classList.remove('main-header--nojs');
+HeaderToggle.classList.remove('main-header--nojs');
 
-    navToggle.addEventListener('click', function() {
-      if (navMain.classList.contains('main-nav--closed')) {
+navToggle.addEventListener('click', function() {
+    if (navMain.classList.contains('main-nav--closed')) {
         navMain.classList.remove('main-nav--closed');
         navMain.classList.add('main-nav--opened');
         HeaderToggle.classList.add('main-header--opened');
-      } else {
+    } else {
         navMain.classList.remove('main-nav--opened');
         HeaderToggle.classList.remove('main-header--opened');
         navMain.classList.add('main-nav--closed');
-      }
-    });
+    }
+});
