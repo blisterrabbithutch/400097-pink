a2ebcf1951092992f6f28dee3a5dad65c21ceb7c
blisterrabbithutch <blisterrabbithutch@gmail.com>
2017-06-23 20:08:06 +0500

    gulp правки

--- gulpfile.js
+++ gulpfile.js
@@ -7,18 +7,36 @@ var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var mqpacker = require("css-mqpacker");
 var minify = require("gulp-csso");
-var imagemin = require("gulp-imagemin");
 var rename = require("gulp-rename");
+var imagemin = require("gulp-imagemin");
+var server = require("browser-sync").create();
 var svgstore = require("gulp-svgstore");
 var svgmin = require("gulp-svgmin");
-var server = require("browser-sync").create();
 var run = require("run-sequence");
 var del = require("del");
-// var sourcemaps = require('gulp-sourcemaps');
+
+gulp.task("build", function (fn) {
+  run("clean", "copy", "style", "images", "symbols", fn);
+});
+
+gulp.task("clean", function() {
+  return del("build");
+});
+
+gulp.task("copy", function() {
+  return gulp.src([
+    "fonts/**/*.{woff,woff2}",
+    "img/**",
+    "js/**",
+    "*.html"
+  ], {
+    base: "."
+  })
+  .pipe(gulp.dest("build"));
+});
 
 gulp.task("style", function() {
   gulp.src("less/style.less")
-    // .pipe(sourcemaps.init())  
     .pipe(plumber())
     .pipe(less())
     .pipe(postcss([
@@ -33,19 +51,8 @@ gulp.task("style", function() {
     .pipe(minify())
     .pipe(rename("style.min.css"))
     .pipe(gulp.dest("build/css")) 
-    // .pipe(sourcemaps.write('css/maps'))
     .pipe(server.stream());
 });
-
-gulp.task("serve", ["style"], function() {
-  server.init({
-    // server:"build/"
-    server: ".",
-    notify: false,
-    open: true,
-    cors: true,
-    ui: false
-  });
 
 gulp.task("images", function()  {
   return gulp.src("build/img/**/*.{png,jpg,gif}")
@@ -56,8 +63,21 @@ gulp.task("images", function()  {
     .pipe(gulp.dest("build/img"));
 });
 
+gulp.task("serve", function() {
+  server.init({
+    server: "build/",
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
+  });
+
+gulp.task("html:copy", function() {
+  return gulp.src("*.html")
+    .pipe(gulp.dest("build"));
+});
+
 gulp.task("symbols", function() {
-  // почему в сурс нужен билд?
   return gulp.src("build/img/icons/*.svg")
     .pipe(svgmin())
     .pipe(svgstore({
@@ -67,39 +87,13 @@ gulp.task("symbols", function() {
     .pipe(gulp.dest("build/img"));
 });
 
-gulp.task("copy", function() {
-  return gulp.src([
-    "fonts/**/*.{woff,woff2}"],
-    "img/**",
-    "js/**",
-    "*.html"
-    // 1-52 синтаксис
-    ], {
-      base: "."
-    })
-    .pipe(gulp.dest("build"));
-});
-
-gulp.task("clean", function() {
-  return del("build");
-});
-
-gulp.task("html:copy", function() {
-  return gulp.src("*.html")
-    .pipe(gulp.dest("build"));
-});
-
 gulp.task("html.update", ["html:copy"], function(done) {
   server.reload();
   done();
 });
 
-gulp.task("build", function (fn) {
-  run("clean", "copy", "style", "images", "symbols", fn);
-});
 
-  gulp.watch("less/**/*.less", ["style"]);
-  gulp.watch("img/*.svg").on("change", server.reload);
-  gulp.watch("*.html").on("change", server.reload);
-  gulp.watch("*.html", ["html:update"]);
+gulp.watch("less/**/*.less", ["style"]);
+gulp.watch("img/*.svg").on("change", server.reload);
+gulp.watch("*.html", ["html:update"]);
 });
